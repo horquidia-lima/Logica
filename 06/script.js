e5503d0f5894e5fff524fae28cb99d6780359a9f
horquidia-lima <horquidia.a.lima@gmail.com>
2023-10-09 22:04:37 -0300

    add

---
+++ 06/script.js
@@ -0,0 +1,17 @@
+/*
+Faca um programa que tenha um menu e apresente a seguinte mensagem:
+
+Ola usuario! Digite a opcao desejada
+
+1 - Cadastrar um item na lista
+2 - Mostrar itens cadastrados
+3 - Sair do programa
+
+O programa devera capturar o numero digitado pelo usuario e mostrar o seguintes cenarios:
+
+Caso o usuario digite 1, ele podera cadastrar um item em uma lista
+Caso o usuario digite 2, ele podera ver os itens cadastrados
+  Se nao houver nenhum item cadastrado, mostrar a mensage:
+    "Nao existem itens cadastrados"
+  Caso o usuario digite 3, a aplicacao devera ser encerada
+*/
