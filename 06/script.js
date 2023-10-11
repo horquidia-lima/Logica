fa6d89ea26fc58049494cbc16dc5a137e70c0acd
horquidia-lima <horquidia.a.lima@gmail.com>
2023-10-10 21:08:14 -0300

    add nueva logica

--- 06/script.js
+++ 06/script.js
@@ -26,17 +26,23 @@ while(option != 3){
   2 - Mostrar itens cadastrados
   3 - Sair do programa
   `))
-  
-  if(option == 1){
-    let item = prompt('Digite o nome do item')
-    items.push(item)
-  }else if(option == 2){
-    if(items.length == 0){
-      alert("Nao existem itens cadastrados")
-    }else {
-      alert(items)
-    }
-  }else {
-    alert('Tchau')
+
+  switch(option) {
+    case 1:
+      let item = prompt('Digite o nome do item')
+      items.push(item)
+      break
+    case 2:
+      if(items.length == 0){
+        alert("Nao existem itens cadastrados")
+      }else {
+        alert(items)
+      }
+      break
+    case 3:
+      alert('Tchau')
+      break
+    default:
+      alert('Opcao invalida! Tente novamente') 
   }
 }
